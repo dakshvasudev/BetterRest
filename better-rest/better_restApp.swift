9bb194d25f8d0a59c6ee4f11ef83e104ed95c706
dakshvasudev <91746245+dakshvasudev@users.noreply.github.com>
2024-05-25 00:19:10 +0530

    Initial Commit

---
+++ better-rest/better_restApp.swift
@@ -0,0 +1,17 @@
+//
+//  better_restApp.swift
+//  better-rest
+//
+//  Created by daksh vasudev on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct better_restApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
